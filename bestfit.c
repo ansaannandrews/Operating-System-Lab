c763d4ce10b685c37438576dfd10828e69d5cc87
ansaannandrews <ansaannandrews2027@cs.sjcetpalai.ac.in>
2025-02-24 23:52:06 +0530

    Update bestfit.c

--- bestfit.c
+++ bestfit.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 
 void main() {
-    int mb, ps, i, j;
+    int mb, ps, i, j, temp;
 
     printf("Enter the Number of Memory Blocks: ");
     scanf("%d", &mb);
 
     struct memory {
-        int size, alloc;
+        int msize, alloc;
     } m[mb];
 
     printf("Enter the size of Each Memory Block:\n");
     for(i = 0; i < mb; i++) {
-        scanf("%d", &m[i].size);
+        scanf("%d", &m[i].msize);
         m[i].alloc = 0;
     }
 
@@ -29,19 +29,35 @@ void main() {
         p[i].flag = 0;
     }
     printf("\n\n");
+    //sorting memory blocks in ascending order
+    for(i = 0;i < mb-1;i++) {
+        for(j = 0;j < mb-1;j++) {
+            if(m[j].msize>m[j+1].msize)
+		{
+			temp=m[j].msize;
+	
+			m[j].msize=m[j+1].msize;
+		
+			m[j+1].msize=temp;
+	
+		}
+	}
+}
+    
 
-    for(i = 0; i < ps; i++) {
-        for(j = 0; j < mb; j++) {
-            if(p[i].flag == 0 && m[j].alloc == 0 && p[i].psize <= m[j].size) {
+    for(i = 0; i < ps; i++) {  // for process
+        int allocated = 0;
+        for(j = 0; j < mb; j++) { // for memory
+            if( m[j].alloc == 0 && p[i].psize <= m[j].msize) {
+                m[j].alloc = p[i].flag;
                 m[j].alloc = 1;
-                p[i].flag = 1;
-                printf("Process %d is allocated in %d block\n", p[i].psize, m[j].size);
+                printf("Process of size %d is allocated in Block of size %d\n", p[i].psize, m[j].msize);
+                allocated = 1;
                 break;
             }
         }
-        if(p[i].flag == 0) {
-            printf("There is no space for %d process!!!!!\n", p[i].psize);
+        if(!allocated) {
+            printf("\nThere is no space for Process of size %d!!!!!\n", p[i].psize);
         }
     }
 }
-
