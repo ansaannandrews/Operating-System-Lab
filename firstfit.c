847d619a345e3237243fca564f552f48150bdd37
ansaannandrews <ansaannandrews2027@cs.sjcetpalai.ac.in>
2025-02-25 13:44:34 +0530

    Update firstfit.c

--- firstfit.c
+++ firstfit.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-void main() {
+int main() {
     int mb, ps, i, j;
 
     printf("Enter the Number of Memory Blocks: ");
@@ -46,61 +46,3 @@ void main() {
         }
     }
 }
-
-
-
-/*#include<stdio.h>
-void main() {
-
-     int mb, ps , i, j;
-     
-     struct memory {
-          int msize, alloc;
-     }m[mb];
-     
-     struct process {
-          int psize, flag;
-     }p[ps];
-     
-     printf("Enter the Number of Memory Blocks: ");
-     scanf("%d",&mb);
-     printf("Enter the size of Each Memory Block:\n");
-     for(i = 0; i < mb; i++) {
-          scanf("%d",&m[i].msize);
-          m[i].alloc = 0;
-     }
-     
-     printf("Enter the Number of Process: ");
-     scanf("%d",&ps);
-     printf("Enter the size of Each Process:\n");
-     for(i = 0; i < ps; i++) {
-          scanf("%d",&p[i].psize);
-          p[i].flag = 0;
-     }
-     
-     for(i = 0; i < ps; i++) {
-          for(j = 0; i < mb; i++) {
-               if(p[i].flag == 0) {
-                    if(p[i].psize <= m[j].msize) {
-                         if(m[j].alloc == 1)
-                              continue;
-                         else {
-                              m[j].alloc = 1;
-                              p[i].flag == 1;
-                              
-                              printf("Process %d is allocated in %d Block\n",p[i].psize,m[j].msize);
-                              }
-                    }
-               }
-          }
-    }
-     
-     for(i = 0; i < ps; i++) {
-         if(p[i].flag == 0) {
-              printf("There is no space for %d process\n",p[i].psize);
-         }
-   }
-}   */
-                              
-                         
-                    
